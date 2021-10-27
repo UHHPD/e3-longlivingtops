8d46cb045a98374a231af1f029bcd49537620de1
Daniel <82508178+TheMaker845@users.noreply.github.com>
2021-10-27 12:05:40 +0000

    part1done

--- poisson.cc
+++ poisson.cc
@@ -9,12 +9,22 @@ double poisson(double mu, int k) {
 
 int main() {
     using namespace std;
-
+    vector<int> zaehler(11);
 
     ifstream fin("datensumme.txt");
+    ofstream fout("hist.txt");
+
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
     }
     fin.close();
+    for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) 
+    {
+      cout << k << ":" << zaehler [ k ] <<  endl ;
+      fout << k<<"\t" << zaehler [ k ] << endl;
+    }
+    fout.close();
 }
+//Hallo Daniel Hallo Cedric
